158371281987a6e2255ffbbabf2595c62b6a7caf
scottsmith3691 <spsmith@NN121-HA-18.HS.sd203.org>
2015-09-11 09:48:32 -0500

    initial
    scott

---
+++ Chapter 3 Practice/Car.java
@@ -0,0 +1,41 @@
+
+
+/**
+ * A car has a certain fuel efficancy and a certain amount 
+ * of fuel in the gas tank. The car can be driven a certain 
+ * distance
+ * 
+ * @author Scott Smith
+ * @version 10 September 2015
+ */
+public class Car
+{
+    /** fuel efficiency of the car in miles per gallon (mpg)*/
+    private double fuelE;
+    
+    /**amount of fuel left in the cars tank in gallons*/
+    private double fuelTank;
+
+    /**
+     * Constructor for objects of class Car that specifies the fuel efficiency
+     */
+    public Car(double fuelE)
+    {
+        // initialise instance variables
+        fuelE = fuelE;
+;
+    }
+
+    /**
+     * Simulates driving a car the specified distance and reduces the amount of fuel in the tank
+     *
+     * @pre        specified distance cannot result in the consumption of more gas than is available
+     * @param    distance the car drives in miles
+     */
+    public void drive(double distance)
+    {
+        // put your code here
+
+    }
+
+}
